339e367f33ead3dee9a14bf94294b2ad6f5bbfdf
Shaker Hossain <73550448+shaker87@users.noreply.github.com>
2021-04-07 17:50:28 +0600

    Create foodData.js

---
+++ foodData.js
@@ -0,0 +1,275 @@
+import b1 from '../images/categoryfood/b1.jpg'
+import b2 from '../images/categoryfood/b2.jpg'
+import b3 from '../images/categoryfood/b3.jpg'
+import b4 from '../images/categoryfood/b4.jpg'
+import b5 from '../images/categoryfood/b5.jpg'
+import b6 from '../images/categoryfood/b6.jpg'
+import b7 from '../images/categoryfood/b2.jpg'
+
+// indina Food
+
+import i1 from '../images/categoryfood/i1.jpg'
+import i2 from '../images/categoryfood/i2.jpg'
+import i3 from '../images/categoryfood/i3.jpg'
+import i4 from '../images/categoryfood/i4.jpg'
+import i5 from '../images/categoryfood/i5.jpg'
+import i6 from '../images/categoryfood/i6.jpg'
+import i7 from '../images/categoryfood/i7.jpg'
+
+
+//chinese food
+import c1 from '../images/categoryfood/c1.jpg'
+import c2 from '../images/categoryfood/c2.jpg'
+import c3 from '../images/categoryfood/c3.jpg'
+import c4 from '../images/categoryfood/c4.jpg'
+import c5 from '../images/categoryfood/c5.jpg'
+import c6 from '../images/categoryfood/c6.jpg'
+
+//thai food
+
+import t1 from '../images/categoryfood/t1.jpg'
+import t2 from '../images/categoryfood/t2.jpg'
+import t3 from '../images/categoryfood/t3.jpg'
+import t4 from '../images/categoryfood/t4.jpg'
+import t5 from '../images/categoryfood/t5.jpg'
+import t6 from '../images/categoryfood/t6.jpg'
+
+
+
+
+export const foods = [
+        {
+                "id": 0,
+                "category": "indian",
+                "name": "Indian Food 1",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 6.9,
+                "rating": 4.2,
+                "image": i1
+        },
+        {
+                "id": 1,
+                "category": "bangla",
+                "name": "Bangla food 1",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 8.9,
+                "rating": 4.3,
+                "image": b1
+        },
+        {
+                "id": 2,
+                "category": "bangla",
+                "name": "Bangla Food 2",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 4.9,
+                "rating": 4.1,
+                "image": b2
+        },
+        {
+                "id": 3,
+                "category": "bangla",
+                "name": "Bangla Food 3",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 4.9,
+                "rating": 4.1,
+                "image": b3
+        },
+        {
+                "id": 4,
+                "category": "bangla",
+                "name": "Bangla Food 4",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 5.6,
+                "rating": 4.3,
+                "image": b4
+        },
+        {
+                "id": 5,
+                "category": "indian",
+                "name": "Indian Food 2",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 4.6,
+                "rating": 4.3,
+                "image": i2
+        },
+        {
+                "id": 6,
+                "category": "chinese",
+                "name": "Chinese Food 1",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 9.6,
+                "rating": 4.1,
+                "image": c1
+        },
+        {
+                "id": 7,
+                "category": "thai",
+                "name": "Thai Food 1",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 7.6,
+                "rating": 4.3,
+                "image": t1
+        },
+        {
+                "id": 8,
+                "category": "bangla",
+                "name": "Bangla Food 5",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 8.6,
+                "rating": 4.6,
+                "image": b5
+        },
+        {
+                "id": 9,
+                "category": "indian",
+                "name": "Inidian Food 3",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 7.4,
+                "rating": 4.6,
+                "image": i3
+        },
+        {
+                "id": 10,
+                "category": "chinese",
+                "name": "Chinese Food 2",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 6.9,
+                "rating": 4.6,
+                "image": c2
+        },
+        {
+                "id": 11,
+                "category": "thai",
+                "name": "Thai Food 2",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 9.9,
+                "rating": 4.7,
+                "image": t2
+        },
+        {
+                "id": 12,
+                "category": "bangla",
+                "name": "Bangla Food 6",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 6.9,
+                "rating": 4.2,
+                "image": b6
+        },
+        {
+                "id": 13,
+                "category": "indian",
+                "name": "Indian Food 4",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 5.9,
+                "rating": 4.2,
+                "image": i4
+        },
+        {
+                "id": 14,
+                "category": "chinese",
+                "name": "Chinese Food 3",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 8.9,
+                "rating": 4.2,
+                "image": c3
+        },
+        {
+                "id": 15,
+                "category": "thai",
+                "name": "Thai Food 3",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 7.9,
+                "rating": 4.6,
+                "image": t3
+        },
+        {
+                "id": 16,
+                "category": "indian",
+                "name": "Indian Food 5",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 8.9,
+                "rating": 4.4,
+                "image": i5
+        },
+        {
+                "id": 17,
+                "category": "bangla",
+                "name": "Bangla Food 7",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 8.9,
+                "rating": 4.4,
+                "image": b7
+        },
+        {
+                "id": 18,
+                "category": "indian",
+                "name": "Indian Food 6",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 4.6,
+                "rating": 4.3,
+                "image": i6
+        },
+        {
+                "id": 19,
+                "category": "indian",
+                "name": "Indian Food 7",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 4.6,
+                "rating": 4.3,
+                "image": i7
+        },
+        {
+                "id": 20,
+                "category": "chinese",
+                "name": "Chinese Food 4",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 8.9,
+                "rating": 4.2,
+                "image": c4
+        },
+        {
+                "id": 21,
+                "category": "chinese",
+                "name": "Chinese Food 5",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 8.9,
+                "rating": 4.2,
+                "image": c5
+        },
+        {
+                "id": 22,
+                "category": "chinese",
+                "name": "Chinese Food 6",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 8.9,
+                "rating": 4.2,
+                "image": c6
+        },
+        {
+                "id": 23,
+                "category": "thai",
+                "name": "Thai Food 4",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 7.9,
+                "rating": 4.6,
+                "image": t4
+        },
+        {
+                "id": 24,
+                "category": "thai",
+                "name": "Thai Food 5",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 7.9,
+                "rating": 4.6,
+                "image": t5
+        },
+        {
+                "id": 25,
+                "category": "thai",
+                "name": "Thai Food 6",
+                "details": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "price": 7.9,
+                "rating": 4.6,
+                "image": t6
+        }
+]
